ecdcdd8aba2210b9ddae4d59891be1a527d6c432
Craig Pickles <craig.pickles@invently.co.uk>
2019-09-30 11:31:20 +0100

    Image classification & Sentimenty analysis

---
+++ MachineLearning.ImageClassification/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using System.Collections.Generic;
+
+namespace MachineLearning.ImageClassification
+{
+    class Program
+    {
+        private static readonly string _trainingFolder = Path.Combine(@"..\..\..\data", "Train");
+        private static readonly string _validationFolder = Path.Combine(@"..\..\..\data", "Validate");
+        private static readonly string _modelLocation = Path.Combine(@"..\..\..\data", "model.zip");
+
+        static void Main(string[] args)
+        {
+            var ctx = new MLContext();
+
+            var start = DateTime.Now;
+
+            Train(ctx);
+
+            Console.WriteLine($"Took to train {(DateTime.Now - start).TotalSeconds.ToString()}s");
+            Console.WriteLine($"Saved model to \"{_modelLocation}\"");
+
+            Validation(ctx);
+        }
+
+        private static void Train(MLContext ctx)
+        {
+            var data = ctx.Data.LoadFromEnumerable(ImageData.ReadFromFolder(_trainingFolder));
+            var tensorflowModel = ctx.Model.LoadTensorFlowModel("tensorflow_inception_graph.pb");
+
+            var pipeline = ctx.Transforms.Conversion.MapValueToKey(outputColumnName: "Label", inputColumnName: "Label")
+                .Append(ctx.Transforms.LoadImages(outputColumnName: "input",
+                                                      imageFolder: "",
+                                                      inputColumnName: nameof(ImageData.Location)))
+                .Append(ctx.Transforms.ResizeImages(outputColumnName: "input",
+                                                        imageWidth: ImageNetSettings.imageWidth,
+                                                        imageHeight: ImageNetSettings.imageHeight,
+                                                        inputColumnName: "input"))
+                .Append(ctx.Transforms.ExtractPixels(outputColumnName: "input",
+                                                         interleavePixelColors: ImageNetSettings.channelsLast,
+                                                         offsetImage: ImageNetSettings.mean))
+                .Append(tensorflowModel.ScoreTensorFlowModel(outputColumnNames: new[] { "softmax2_pre_activation" },
+                                                     inputColumnNames: new[] { "input" },
+                                                     addBatchDimensionInput: true))
+                .Append(ctx.MulticlassClassification.Trainers.LbfgsMaximumEntropy(labelColumnName: "Label",
+                                                                                      featureColumnName: "softmax2_pre_activation"))
+                .Append(ctx.Transforms.Conversion.MapKeyToValue("Prediction", "PredictedLabel"));
+
+            var model = pipeline.Fit(data);
+            ctx.Model.Save(model, data.Schema, _modelLocation);
+        }
+
+        private static void Validation(MLContext ctx)
+        {
+            var model = ctx.Model.Load(_modelLocation, out var schema);
+            var data = ctx.Data.LoadFromEnumerable(ImageData.ReadFromFolder(_validationFolder));
+
+            // do the thing!
+            var valData = model.Transform(data);
+
+            // print out results
+            var predictions = ctx.Data.CreateEnumerable<ImageDataPrediction>(valData, false, true);
+            foreach (var pr in predictions)
+                Console.WriteLine($"{pr.Location}, {pr.Prediction}, {pr.Score.Max()}");
+
+            // print metrics
+            var classificationContext = ctx.MulticlassClassification;
+            var metrics = classificationContext.Evaluate(valData, labelColumnName: "Label", predictedLabelColumnName: "PredictedLabel");
+            Console.WriteLine($"LogLoss is: {metrics.LogLoss}");
+            Console.WriteLine($"PerClassLogLoss is: {String.Join(" , ", metrics.PerClassLogLoss.Select(c => c.ToString()))}");
+        }
+    }
+
+    public struct ImageNetSettings
+    {
+        public const int imageHeight = 224;
+        public const int imageWidth = 224;
+        public const float mean = 117;
+        public const float scale = 1;
+        public const bool channelsLast = true;
+    }
+
+    public class ImageData
+    {
+        public string Location { get; set; }
+        public string Label { get; set; }
+
+        public static IEnumerable<ImageData> ReadFromFolder(string folder)
+        {
+            foreach (var name in Directory.EnumerateDirectories(folder))
+            {
+                var label = Path.GetFileName(name);
+                foreach (var f in Directory.EnumerateFiles(name, "*.jpg"))
+                    yield return new ImageData { Location = Path.GetFullPath(f), Label = label };
+            }
+        }
+    }
+
+    public class ImageDataPrediction
+    {
+        public string Location { get; set; }
+        public float[] Score { get; set; }
+        public string Prediction { get; set; }
+    }
+}
---
+++ MachineLearning.SentimentAnalysis/Program.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace MachineLearning.SentimentAnalysis
+{
+    class Program
+    {
+        private static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "amazon_cells_labelled.txt");
+
+        static void Main(string[] args)
+        {
+            var ctx = new MLContext();
+
+            var splitDataView = LoadData(ctx);
+
+            var model = BuildAndTrainModel(ctx, splitDataView.TrainSet);
+
+            Evaluate(ctx, model, splitDataView.TestSet);
+
+            Predict(ctx, model, "This was a horrible meal", "I hate this", "I love this spaghetti");
+
+            Console.ReadKey();
+        }
+
+        private static TrainTestData LoadData(MLContext ctx)
+        {
+            var dataView = ctx.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+
+            var splitDataView = ctx.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            return splitDataView;
+        }
+
+        private static ITransformer BuildAndTrainModel(MLContext ctx, IDataView splitTrainSet)
+        {
+            var estimator = ctx.Transforms.Text
+                .FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText)) // convert each word into a feature
+                .Append(ctx.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(splitTrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+
+            return model;
+        }
+
+        private static void Evaluate(MLContext ctx, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data ===============");
+            var predictions = model.Transform(splitTestSet);
+
+            var metrics = ctx.BinaryClassification.Evaluate(predictions, "Label");
+
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}"); // accuracy of a model
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}"); // how confident the model is correctly classifying the positive and negative classes.
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}"); // measure of balance between precision and recall.
+            Console.WriteLine("=============== End of model evaluation ===============");
+        }
+
+        private static void Predict(MLContext ctx, ITransformer model, params string[] text)
+        {
+            IEnumerable<SentimentData> sentiments = text.Select(t => new SentimentData
+            {
+                SentimentText = t
+            });
+
+            IDataView batchComments = ctx.Data.LoadFromEnumerable(sentiments);
+
+            IDataView predictions = model.Transform(batchComments);
+
+            var predictedResults = ctx.Data.CreateEnumerable<SentimentPrediction>(predictions, reuseRowObject: false);
+
+            Console.WriteLine();
+
+            Console.WriteLine("=============== Predictions ==============="); foreach (SentimentPrediction prediction in predictedResults)
+            {
+                Console.WriteLine($"Sentiment: {prediction.SentimentText} | Prediction: {(Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative")} | Probability: {prediction.Probability} ");
+
+            }
+            Console.WriteLine("=============== End of predictions ===============");
+        }
+    }
+
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public string SentimentText;
+
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Sentiment;
+    }
+
+    public class SentimentPrediction : SentimentData
+    {
+
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
+}
